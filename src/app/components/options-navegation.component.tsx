ef97cf93a7bda6423944c011f54e4186846b305d
Mathias Jave Diaz <u202212721@upc.edu.pe>
2024-09-18 12:06:53 -0500

    chore(router): changed page links in router

--- src/app/components/options-navegation.component.tsx
+++ src/app/components/options-navegation.component.tsx
@@ -5,11 +5,11 @@ import OperationCenter from '../../../assets/operation-center.jpeg'
 import Link from "next/link";
 export function OptionsNavigation(){
     return(
-        <div className="flex flex-col lg:flex-row justify-center items-center gap-9 p-10 lg:p-20 ">
-            <Link className={"hover: scale-110 hover:cursor-pointer hover:border-2 hover:border-black hover:rounded-lg duration-200"} href={"http://38.25.38.94:5173/#/"}>
+        <div className="options-container flex flex-col lg:flex-row justify-center items-center gap-9 p-10 lg:p-20 ">
+            <Link className={"hover: scale-110 hover:cursor-pointer hover:border-2 hover:border-black hover:rounded-lg duration-200"} href={"http://hiperfastservice.pe:5173/#/"}>
                 <Image className={"min-w-52"} height={200}  src={Cylinder} alt={"Cylinder Option"}/>
             </Link>
-            <Link className={"hover: scale-110 hover:cursor-pointer hover:border-2 hover:border-black hover:rounded-lg duration-200"} href={"http://38.25.38.94:5260/"}>
+            <Link className={"hover: scale-110 hover:cursor-pointer hover:border-2 hover:border-black hover:rounded-lg duration-200"} href={"http://hiperfastservice.pe:5260/#/"}>
                 <Image className={"min-w-52"} height={200}  src={OperationCenter} alt={"Operation Center Option"}/>
             </Link>
         </div>
